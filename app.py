46c4c7e69c8471970f9066391653b3ecc01b9b08
sid-38 <sidharthanil98@gmail.com>
2021-03-14 21:33:28 +0530

    Paillier initial encryption

--- app.py
+++ app.py
@@ -1,8 +1,27 @@
-from flask import Flask, request;
+from flask import Flask, request
+from phe import paillier
+import pickle
+import json
 app = Flask(__name__)
+private_key = None
+public_key = None
+
+with open("private_key",'rb') as pvt_file, open("public_key",'rb') as pub_file:
+    private_key = pickle.load(pvt_file)
+    public_key = pickle.load(pub_file)
 
 @app.route('/verify', methods=['POST'])
 def verify():
-    name = request.get_json();
-    print(name);
-    return({'message':'success'},200);
+    data = request.get_json();
+    if not 'fp' in data:
+        return({'message':'error'},400)
+    fp = data['fp']
+    efp = []
+    for item in fp:
+        efp.append(public_key.encrypt(item))
+
+    print(efp)
+    return({'message':'success'},200)
+
+
+
